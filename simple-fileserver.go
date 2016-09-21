889f72a6205195f04dc9abe8930971a971e11c2b
Georgij Grigorev <grigorievga@yandex.ru>
2016-09-21 16:39:48 +0300

    Compiled src

--- simple-fileserver.go
+++ simple-fileserver.go
@@ -5,7 +5,6 @@ import (
         "net/http"
         )
 func main()  {
-    
     port := flag.String("p", "8080", "Порт для запуска сервера")
     directory := flag.String("d", ".", "Директория для отображения файлов")
     flag.Parse()
